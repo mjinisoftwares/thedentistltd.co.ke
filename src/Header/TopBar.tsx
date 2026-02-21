fc8a27b9669998cbaddf2302833ab94678728288
mjini softwares <mjinisoftwares@gmail.com>
2026-02-21 11:42:11 +0300

    cc

--- src/Header/TopBar.tsx
+++ src/Header/TopBar.tsx
@@ -22,12 +22,12 @@ export function TopBar({ hidden }: TopBarProps) {
             className="flex items-center gap-2 hover:text-secondary"
           >
             <Mail className="w-4 h-4" />
-            <span className="hidden sm:inline">info@dentist.com</span>
+            <span className="hidden sm:inline">info@thedentistltd.co.ke</span>
           </a>
 
           <a href="tel:+1234567890" className="flex items-center gap-2 hover:text-secondary">
             <Phone className="w-4 h-4" />
-            <span className="hidden sm:inline">+123 456 7890</span>
+            <span className="hidden sm:inline">+254734003111</span>
           </a>
         </div>
 
@@ -35,21 +35,18 @@ export function TopBar({ hidden }: TopBarProps) {
         <div className="flex items-center gap-6">
           <div className="hidden md:flex items-center gap-2">
             <MapPin className="w-4 h-4" />
-            <span>123 Dental Street</span>
+            <span>West Road, D&D House - 6th floor</span>
           </div>
 
           <div className="flex items-center gap-3">
-            <Link href="#">
+            <Link href="https://www.facebook.com/thedentistke">
               <Facebook className="w-4 h-4" />
             </Link>
-            <Link href="#">
+            <Link href="https://x.com/DentistLtd">
               <Twitter className="w-4 h-4" />
             </Link>
-            <Link href="#">
+            <Link href="https://www.instagram.com/the_dentist_ltd/?hl=en">
               <Instagram className="w-4 h-4" />
-            </Link>
-            <Link href="#">
-              <Linkedin className="w-4 h-4" />
             </Link>
           </div>
         </div>
